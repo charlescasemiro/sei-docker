b3944e2c7db3e525a5f79b6239611936f65ced72
charlescasemiro <49919162+charlescasemiro@users.noreply.github.com>
2024-07-09 21:01:25 +0100

    Upload files

--- containers/app/app-base/assets/copy-packages.sh
+++ containers/app/app-base/assets/copy-packages.sh
@@ -6,16 +6,14 @@ mkdir -p /tmp/assets/pacotes
 cd /tmp/assets/pacotes
 
 curl -L -O \
-    https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/msttcore-fonts-2.0-3.noarch.rpm \
+    https://github.com/charlescasemiro/sei-docker-binarios/raw/main/pacoteslinux/msttcore-fonts-2.0-3.noarch.rpm \
     -O \
-    https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/oracle-instantclient12.2-basic-12.2.0.1.0-1.x86_64.rpm \
+    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-precomp-linux.leppc64.c64-19.3.0.0.0dbru.zip \
     -O \
-    https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/oracle-instantclient12.2-devel-12.2.0.1.0-1.x86_64.rpm \
+    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sdk-aix.ppc64-19.23.0.0.0dbru.zip \
     -O \
-    https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/oracle-instantclient12.2-sqlplus-12.2.0.1.0-1.x86_64.rpm \
+    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sqlplus-aix.ppc64-19.23.0.0.0dbru.zip \
     -O \
-    https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/uploadprogress.tgz \
-    -O \
-    https://github.com/spbgovbr/sei-docker-binarios/raw/main/pacoteslinux/wkhtmltox-0.12.6-1.centos7.x86_64.rpm 
+    https://github.com/charlescasemiro/sei-docker-binarios/raw/main/pacoteslinux/uploadprogress.tgz
 
 cd -
