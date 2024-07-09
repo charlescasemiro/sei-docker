7a3511b8d728ed032ccfa9626dc03e131755d810
charlescasemiro <49919162+charlescasemiro@users.noreply.github.com>
2024-07-09 21:17:29 +0100

    download pacotes

--- containers/app/app-base/assets/copy-packages.sh
+++ containers/app/app-base/assets/copy-packages.sh
@@ -3,15 +3,10 @@
 mkdir -p /tmp/assets/pacotes
 cd /tmp/assets/pacotes
 
-curl -L -O \
-    https://github.com/charlescasemiro/sei-docker-binarios/raw/main/pacoteslinux/msttcore-fonts-2.0-3.noarch.rpm \
-    -O \
-    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-precomp-linux.leppc64.c64-19.3.0.0.0dbru.zip \
-    -O \
-    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sdk-aix.ppc64-19.23.0.0.0dbru.zip \
-    -O \
-    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sqlplus-aix.ppc64-19.23.0.0.0dbru.zip \
-    -O \
-    https://github.com/charlescasemiro/sei-docker-binarios/raw/main/pacoteslinux/uploadprogress.tgz
+wget https://github.com/charlescasemiro/sei-docker-binarios/raw/main/pacoteslinux/msttcore-fonts-2.0-3.noarch.rpm
+wget https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-precomp-linux.leppc64.c64-19.3.0.0.0dbru.zip
+wget https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sdk-aix.ppc64-19.23.0.0.0dbru.zip
+wget https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sqlplus-aix.ppc64-19.23.0.0.0dbru.zip
+wget https://github.com/charlescasemiro/sei-docker-binarios/raw/main/pacoteslinux/uploadprogress.tgz
 
 cd -
