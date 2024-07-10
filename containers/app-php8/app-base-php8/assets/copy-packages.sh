c6a08ff0915e693e6d7958898efa61cf776ee204
charlescasemiro <49919162+charlescasemiro@users.noreply.github.com>
2024-07-10 16:19:52 +0100

    update files

--- containers/app-php8/app-base-php8/assets/copy-packages.sh
+++ containers/app-php8/app-base-php8/assets/copy-packages.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-#!/bin/bash
-
 mkdir -p /tmp/assets/pacotes
 cd /tmp/assets/pacotes
 
-wget https://github.com/charlescasemiro/sei-docker-binarios/raw/main/pacoteslinux/msttcore-fonts-2.0-3.noarch.rpm
-wget https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-precomp-linux.leppc64.c64-19.3.0.0.0dbru.zip
-wget https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sdk-aix.ppc64-19.23.0.0.0dbru.zip
-wget https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sqlplus-aix.ppc64-19.23.0.0.0dbru.zip
-wget https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/uploadprogress-2.0.2.tgz
+curl -L -O \
+    https://github.com/charlescasemiro/sei-docker-binarios/raw/main/pacoteslinux/msttcore-fonts-2.0-3.noarch.rpm \
+    -O \
+    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-precomp-linux.leppc64.c64-19.3.0.0.0dbru.zip \
+    -O \
+    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sdk-aix.ppc64-19.23.0.0.0dbru.zip \
+    -O \
+    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/instantclient-sqlplus-aix.ppc64-19.23.0.0.0dbru.zip \
+    -O \
+    https://github.com/charlescasemiro/sei-docker-binarios/blob/main/pacoteslinux/uploadprogress-2.0.2.tgz \
 
 cd -
