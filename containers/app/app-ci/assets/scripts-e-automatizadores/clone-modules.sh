a34125661ceae3ed9a511e4f6d45e1257de1f762
charlescasemiro <49919162+charlescasemiro@users.noreply.github.com>
2024-07-11 01:50:28 +0100

    update files

--- containers/app/app-ci/assets/scripts-e-automatizadores/clone-modules.sh
+++ containers/app/app-ci/assets/scripts-e-automatizadores/clone-modules.sh
@@ -4,13 +4,13 @@ mkdir -p /sei-modulos
 
 cd /sei-modulos
 
-git clone https://github.com/supergovbr/mod-sei-estatisticas.git
-git clone https://github.com/supergovbr/mod-sei-pen.git
-git clone https://github.com/supergovbr/mod-wssei.git
+git clone https://github.com/pengovbr/mod-sei-estatisticas.git
+git clone https://github.com/pengovbr/mod-sei-pen.git
+git clone https://github.com/pengovbr/mod-wssei.git
 git clone https://github.com/anatelgovbr/mod-sei-peticionamento.git peticionamento
-git clone https://github.com/supergovbr/mod-sei-resposta.git
-git clone https://github.com/supergovbr/mod-sei-protocolo-integrado.git
-git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/supergovbr/mod-sei-incom.git
-git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/supergovbr/mod-gestao-documental.git
-git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/supergovbr/mod-sei-loginunico.git
-git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/supergovbr/mod-sei-assinatura-avancada.git
+git clone https://github.com/pengovbr/mod-sei-resposta.git
+git clone https://github.com/pengovbr/mod-sei-protocolo-integrado.git
+git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/pengovbr/mod-sei-incom.git
+git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/pengovbr/mod-gestao-documental.git
+git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/pengovbr/mod-sei-loginunico.git
+git clone https://${GITUSER_REPO_MODULOS}:${GITPASS_REPO_MODULOS}@github.com/pengovbr/mod-sei-assinatura-avancada.git
